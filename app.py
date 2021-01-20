45e6a96273e4724424cc8fe1a312135e3fc782b7
Marc Falzon <Marc.Falzon@mail.weir>
2021-01-20 13:52:45 +1100

    First commit - base FastAPI

---
+++ app.py
@@ -0,0 +1,50 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import requests
+
+
+app = FastAPI()
+
+db = []
+
+class City(BaseModel):
+    name: str
+    timezone: str
+
+
+@app.get('/')
+def index():
+    return {'key': 'value'}
+
+
+@app.get('/cities')
+def get_cities():
+    results = []
+    for city in db:
+        r = requests.get(f"http://worldtimeapi.org/api/timezone/{city['timezone']}")
+        r.raise_for_status()
+        current_time = r.json()['datetime']
+        results.append({'name': city['name'], 'timezone': city['timezone'],
+         'current_time': current_time})
+    return results
+
+
+@app.get('/cities/{city_id}')
+def get_city(city_id: int):
+    return db[city_id - 1]
+
+
+
+@app.post('/cities')
+def create_city(city: City):
+    db.append(city.dict())
+    return db[-1]
+
+
+
+
+
+@app.delete('/cities/{city_id}')
+def delete_city(city_id: int):
+    db.pop(city_id-1)
+    return {}
---
+++ second.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+#import saved ML model and vector
+import pickle
+nb_classifier = pickle.load(open('./Count_model.sav', 'rb'))
+vector = pickle.load(open('./count_vector.sav', 'rb'))
+
+@app.get('/')
+def get(text: str):    
+    text = text.lower().replace('[^a-zA-Z\s\@]', '')
+    vectored_new = vector.transform([text])
+    pred = nb_classifier.predict(vectored_new)
+    p_prob = nb_classifier.predict_proba(vectored_new)[0]
+
+    return {'prediction':str(pred[0]),'probability': f"{max(p_prob)*100:.2f}%"}
